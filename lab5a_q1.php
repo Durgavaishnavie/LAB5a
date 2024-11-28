684fb38f139d222fbb395a2fe204e89d975bdacf
Durgavaishnavie <durgavaishnavie@gmail.com>
2024-11-28 09:21:32 +0800

    Coding for lab5a

---
+++ lab5a_q1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q1</title>
+</head>
+<body>
+
+ <?php
+ $name = "Durgavaishnavie a/p Jeyasangar";
+ $matricnumber = "CI230113"
+ $course = "Bachelor of Computer Science (Web Technology) With Honours"
+ $yearofstudy = "Year 2"
+ $address = "No 17V, Jalan Seroja 3, Taman Pusing Baru, 31550 Pusing, Perak"
+ ?>
+
+ <table>
+ <tr>
+ <td>Name</td>
+ <td><?php echo "$name"; ?></td>
+ <td>Matric Number</td>
+ <td><?php echo "$matricnumber"; ?></td>
+ <td>Course</td>
+ <td><?php echo "$course"; ?></td>
+ <td>Year of Study</td>
+ <td><?php echo "$yearofstudy"; ?></td>
+ <td>Address</td>
+ <td><?php echo "$address"; ?></td>
+
+ </tr>
+ </table>
+
+</body>
+</html>
