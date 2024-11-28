b6f46919d16610ac4bc699b2933df6460d0a69f7
Durgavaishnavie <durgavaishnavie@gmail.com>
2024-11-28 10:56:30 +0800

    Update lab5a_q3.php

--- lab5a_q3.php
+++ lab5a_q3.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <title>Lab 5a Q3</title>
+    <title>Lab 5a Q3</title>
 </head>
 <body>
 
-<?php
+    <?php
+    function calculateArea($length, $width) {
+        return $length * $width;
+    }
 
-function calculateArea($length, $width) {
-    return $length * $width;
-}
+    $length = 4;
+    $width = 2;
+    $area = calculateArea($length, $width);
 
-$length = 4;
-$width = 2;
-$area = calculateArea($length, $width);
-
-echo "The area of the rectangle with length $length and width $width is $area.";
-?>
-
+    echo "<b>The area of the rectangle with length $length and width $width is $area.<b>";
+    ?>
 
 </body>
 </html>
