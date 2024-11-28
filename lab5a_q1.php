5d6fa739ed694901b3ca1c42a80ce184463c1b1d
Durgavaishnavie <durgavaishnavie@gmail.com>
2024-11-28 09:28:11 +0800

    Update lab5a_q1.php

--- lab5a_q1.php
+++ lab5a_q1.php
@@ -2,32 +2,51 @@
 <html lang="en">
 <head>
     <title>Lab 5a Q1</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+        }
+        table, th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
-
- <?php
- $name = "Durgavaishnavie a/p Jeyasangar";
- $matricnumber = "CI230113";
- $course = "Bachelor of Computer Science (Web Technology) With Honours";
- $yearofstudy = "Year 2";
- $address = "No 17V, Jalan Seroja 3, Taman Pusing Baru, 31550 Pusing, Perak";
- ?>
-
- <table>
- <tr>
- <td>Name</td>
- <td><?php echo "$name"; ?></td>
- <td>Matric Number</td>
- <td><?php echo "$matricnumber"; ?></td>
- <td>Course</td>
- <td><?php echo "$course"; ?></td>
- <td>Year of Study</td>
- <td><?php echo "$yearofstudy"; ?></td>
- <td>Address</td>
- <td><?php echo "$address"; ?></td>
-
- </tr>
- </table>
-
+    <?php
+    $name = "Durgavaishnavie a/p Jeyasangar";
+    $matricNumber = "CI230113";
+    $course = "Bachelor of Computer Science (Web Technology) With Honours";
+    $yearOfStudy = "Year 2";
+    $address = "No 17V, Jalan Seroja 3, Taman Pusing Baru, 31550 Pusing, Perak";
+    ?>
+    <table>
+        <tr>
+            <th>Detail</th>
+            <th>Information</th>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>Matric Number</td>
+            <td><?php echo $matricNumber; ?></td>
+        </tr>
+        <tr>
+            <td>Course</td>
+            <td><?php echo $course; ?></td>
+        </tr>
+        <tr>
+            <td>Year of Study</td>
+            <td><?php echo $yearOfStudy; ?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php echo $address; ?></td>
+        </tr>
+    </table>
 </body>
 </html>
